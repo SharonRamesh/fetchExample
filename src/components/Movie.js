e556c5a8e4138a62f6560e0d2eaf4f93ddcaa679
Sharon P <sp@morganmckinley.com>
2021-08-17 19:18:18 +0530

    updated codes for fetch example

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
   const [readMore, readHandler] = useState(true);
   const[readValue, valueUpdate] = useState(props.openingText.slice(0,250)+'...');
+  const itemsRef = useRef();
+
+  // useEffect(() => {
+  //   itemsRef.current = itemsRef.current.slice(0, props.items.length);
+  // }, [props.items]);
 
   const clickHandler = () => {
     if(readMore){
       readHandler(false);
       valueUpdate(props.openingText);
+      itemsRef.current.scrollIntoView();    
     }
     else{
       readHandler(true);
       valueUpdate(props.openingText.slice(0,250)+'...');
+      itemsRef.current.scrollIntoView();    
     }
   }
 
   return (
-    <li className={classes.movie}>
+    <li className={classes.movie} ref={itemsRef}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{readValue}</p>
